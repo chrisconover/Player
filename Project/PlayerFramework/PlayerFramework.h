f428546183c501ccd1319884f01813df30abd870
Chris Conover <crconover@gmail.com>
2015-10-01 22:34:11 -0700

    Adding a shared framework scheme

---
+++ Project/PlayerFramework/PlayerFramework.h
@@ -0,0 +1,19 @@
+//
+//  PlayerFramework.h
+//  PlayerFramework
+//
+//  Created by Chris Conover on 10/1/15.
+//  Copyright © 2015 Patrick Piemonte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlayerFramework.
+FOUNDATION_EXPORT double PlayerFrameworkVersionNumber;
+
+//! Project version string for PlayerFramework.
+FOUNDATION_EXPORT const unsigned char PlayerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayerFramework/PublicHeader.h>
+
+
